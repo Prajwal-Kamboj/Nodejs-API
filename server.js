ade7687225677e1beba21f2ab5465ab05a8307de
Prajwal-Kamboj <Prajwal-Kamboj@users.noreply.github.com>
2020-02-25 08:14:18 +0530

    Update querries

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const dotenv = require ('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db')
 
 // load env 
 dotenv.config({path : './config/config.env'});
 
+//connect to db
+connectDB();
+
 const app = express();
+//route files
+const bootcamps = require('./routes/bootcamps');
+
+// Body parser
+app.use(express.json());
+
+
+
+
+// mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT= process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// handle unhandled rejections
+process.on('unhandledRejection',(err, promise) =>{
+    console.log(`Error : ${err.message}`);
+    //close server and exit process
+    server.close(()=> process.exit(1));
+})
